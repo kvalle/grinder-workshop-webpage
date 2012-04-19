445ca85c6952bd17e94176fbc9c65f9c53888af6
Espen Herseth Halvorsen <espen.halvorsen@bekk.no>
2012-04-19 16:15:49 +0200

    Ooooh, teh phun!

--- json.php
+++ json.php
@@ -1,5 +1,5 @@
 <? 
 header('Content-type: application/json');
-$myTweets = '{"fetched":"12.02.2012","tweets":[{"user":"Espenhh","tweet":"Omg, this grinder workshop is amazing","profile_image":"http://grinder.espenhh.com/pics/1337.jpg"},{"user":"kvalle","tweet":"Have you seen my new ROFL-copter? It`s kinda awesome!","profile_image":"http://grinder.espenhh.com/pics/31337.jpg"},{"user":"Hurtigruta","tweet":"You guys rocked my boat!","profile_image":"http://grinder.espenhh.com/pics/123.jpg"}]}';
+$myTweets = '{"fetched":"19.04.2012","tweets":[{"user":"Espenhh","tweet":"Omg, this grinder workshop is amazing","profile_image":"http://grinder.espenhh.com/pics/1337.jpg"},{"user":"kvalle","tweet":"Have you seen my new ROFL-copter? It`s kinda awesome!","profile_image":"http://grinder.espenhh.com/pics/31337.jpg"},{"user":"Bergen","tweet":"Why does it always rain on me??!","profile_image":"http://grinder.espenhh.com/pics/123.jpg"}]}';
 echo $myTweets;
 ?>
