d127bd2928123198a8cbad596c26eb515b68cf7b
Espen Herseth Halvorsen <espen.halvorsen@bekk.no>
2012-05-09 15:01:55 +0200

    Tweaker for testkonferansen ;)

--- json.php
+++ json.php
@@ -1,5 +1,5 @@
 <? 
 header('Content-type: application/json');
-$myTweets = '{"fetched":"19.04.2012","tweets":[{"user":"Espenhh","tweet":"Omg, this grinder workshop is amazing","profile_image":"http://grinder.espenhh.com/pics/1337.jpg"},{"user":"kvalle","tweet":"Have you seen my new ROFL-copter? It`s kinda awesome!","profile_image":"http://grinder.espenhh.com/pics/rofl.gif"},{"user":"Bergen","tweet":"Why does it always rain on me??!","profile_image":"http://grinder.espenhh.com/pics/123.jpg"}]}';
+$myTweets = '{"fetched":"19.04.2012","tweets":[{"user":"Espenhh","tweet":"Omg, this grinder workshop is amazing","profile_image":"http://grinder.espenhh.com/pics/1337.jpg"},{"user":"kvalle","tweet":"Have you seen my new ROFL-copter? It`s kinda awesome!","profile_image":"http://grinder.espenhh.com/pics/rofl.gif"},{"user":"Hurtigruta","tweet":"I`m on a boat! Yeah!","profile_image":"http://grinder.espenhh.com/pics/123.jpg"}]}';
 echo $myTweets;
 ?>
